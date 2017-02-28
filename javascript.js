8722998af4e66b062a74617cc019d098bf2279a5
EMilborn <emilborn@stuy.edu>
2017-02-28 14:50:01 -0500

    bleh

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ var prev = [];
 var addDot = function(e) {
   x = e.offsetX;
   y = e.offsetY;
-  circ=createElementNS(“http://www.w3.org/2000/svg”, circle);
+  circ=createElementNS(“http://www.w3.org/2000/svg”, "circle");
   circ.setAttribute("r", "25");
   s.appendChild(c);
 };
