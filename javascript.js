2afe662af28fc1889aff23e827f2ab039bdd1856
Elias Milborn <emilborn@stuy.edu>
2017-03-01 07:03:37 -0500

    done

--- javascript.js
+++ javascript.js
@@ -1,11 +1,36 @@
 var prev = [];
 
 var addDot = function(e) {
-  x = e.offsetX;
-  y = e.offsetY;
-  circ=createElementNS(“http://www.w3.org/2000/svg”, "circle");
-  circ.setAttribute("r", "25");
-  s.appendChild(c);
+    x = e.offsetX;
+    y = e.offsetY;
+    circ=document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circ.setAttribute("r", "10");
+    circ.setAttribute("cx", x);
+    circ.setAttribute("cy", y);
+    s.appendChild(circ);
+    console.log("addDot");
+    if (prev.length == 2){
+	line=document.createElementNS("http://www.w3.org/2000/svg", "line");
+	line.setAttribute("x1", prev[0]);
+	line.setAttribute("y1", prev[1]);
+	line.setAttribute("x2", x);
+	line.setAttribute("y2", y);
+	line.setAttribute("style", "stroke:rgb(0,0,0);stroke-width:2");
+	s.appendChild(line);
+	console.log("added line");
+    };
+    prev = [x, y];
 };
 
-xd.addEventListener("click", addDot);
+var clear = function(e){
+    prev = [];
+    while (s.firstChild){
+	s.removeChild(s.firstChild);
+    };
+    
+    
+};
+    
+
+s.addEventListener("click", addDot);
+b.addEventListener("click", clear);
